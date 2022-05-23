0b45b222a2653183621eac3c3f303cbbe7335b31
KimYeongYeon <dudtbd111@naver.com>
2022-05-23 15:06:01 +0900

    [Inflearn_basic] feature: Spring Boot 입문 - Gradle 프로젝트 생성 - MavenCentral 및 thymeleaf

---
+++ SpringBoot_Inflearn_Basic/src/test/java/kyy/api/basic/SpringBootInflearnBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package kyy.api.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInflearnBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
