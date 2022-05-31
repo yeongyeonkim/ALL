ef857f209627f1d8946c48cc8d13cf2e4e0003b7
yeongyeonkim <dudtbd111@naver.com>
2022-05-31 16:44:07 +0900

    [SpringCloudMSA] feature : Eureka 사용하기

--- SpringCloudMSA/SpringDiscoveryService/src/main/java/com/api/springdiscoveryservice/SpringDiscoveryServiceApplication.java
+++ SpringCloudMSA/SpringDiscoveryService/src/main/java/com/api/springdiscoveryservice/SpringDiscoveryServiceApplication.java
@@ -3,11 +3,18 @@ package com.api.springdiscoveryservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class SpringDiscoveryServiceApplication {
 
+    @RequestMapping("/")
+    public String home() {
+        return "hello";
+    }
     public static void main(String[] args) {
         SpringApplication.run(SpringDiscoveryServiceApplication.class, args);
     }
