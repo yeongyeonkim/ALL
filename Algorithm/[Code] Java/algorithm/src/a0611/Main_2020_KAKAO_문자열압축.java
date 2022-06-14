555e74d7563712a086d78a990e3aaff97357f8e5
KimYeongYeon <dudtbd111@naver.com>
2022-06-15 00:00:37 +0900

    [Algorithm] feature: 알고리즘 풀이, 드디어 문자열을 해결했다.

--- Algorithm/[Code] Java/algorithm/src/a0611/Main_2020_KAKAO_문자열압축.java
+++ Algorithm/[Code] Java/algorithm/src/a0611/Main_2020_KAKAO_문자열압축.java
@@ -1,53 +1,34 @@
 package a0611;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
-
 public class Main_2020_KAKAO_문자열압축 {
 
 	public static void main(String[] args) {
+		System.out.println(solution("a"));
 		System.out.println(solution("aabbaccc"));
-//		System.out.println(solution("ababcdcdababcdcd"));
-//		System.out.println(solution("abcabcdede"));
-//		System.out.println(solution("abcabcabcabcdededededede"));
-//		System.out.println(solution("xababcdcdababcdcd"));
+		System.out.println(solution("ababcdcdababcdcd"));
+		System.out.println(solution("abcabcdede"));
+		System.out.println(solution("abcabcabcabcdededededede"));
+		System.out.println(solution("xababcdcdababcdcd"));
 	}
 
 	public static int solution(String s) {
-		int answer = Integer.MAX_VALUE/2;
-//		for (int k = 1; k <= s.length() / 2; k++) {
-		for (int k = 1; k <= 1; k++) {
-			Stack<String> st = new Stack<>();
-			Map<String, Integer> map = new HashMap<>();
-			StringBuilder sb = new StringBuilder();
-			int idx = 0;
-			while (true) {
-				if (idx + k > s.length()) { // 범위
-					break;
+		int answer = s.length(); // s.length가 최대 값 일 것 + 길이가 1일 때 아래의 케이스가 작동하지 않음. 잡아내면 더 좋았을 텐데
+		for (int i = 1; i <= s.length() / 2; i++) {
+			int cnt = 1;
+			String begin = s.substring(0, i);
+			StringBuilder result = new StringBuilder();
+			for(int j=i; j<=s.length(); j+=i) {
+				String now = s.substring(j, j + i > s.length() ? s.length() : i + j); // 인덱스 벗어나는 경우
+				// 넘는 경우 s.substring(j) 로 begin 인덱스로 끝까지 찾아지게끔 해도 무관하다.
+				if(begin.equals(now)) cnt++;
+				else {
+					result.append((cnt != 1 ? cnt : "") + begin);
+					begin = now;
+					cnt = 1;
 				}
-				String tmp = s.substring(idx, idx + k);
-				if (!st.isEmpty()) {
-					if (st.peek().equals(tmp)) { // 기존 값과 같으면 해당 String에 대한 갯수를 갱신해 줌
-						map.put(tmp, map.get(tmp) + 1);
-					} else { // 기존 값과 다르면 map에 갯수와 해당 문자를 StringBuilder에 ++
-						String before = st.pop();
-						sb.append(map.get(before));
-						map.put(tmp, 0);
-						sb.append(before);
-						st.add(tmp);
-					}
-				} else {
-					st.add(tmp);
-					map.put(tmp, 1);
-				}
-				idx += k;
-				continue;
 			}
-			System.out.println(k + "gae :" + sb.toString());
-			// aaaaabbbbb인경우 stack에 있는 값 빼주는 작업 필요
-			int sb_length = sb.toString().length();
-			answer = sb_length > answer ? answer : sb_length;
+			result.append(begin);
+			answer = result.length() < answer ? result.length() : answer; 
 		}
 		return answer;
 	}
