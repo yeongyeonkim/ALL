a2c62dbfc4e2e5496b1102a257d3982ec734f226
KimYeongYeon <dudtbd111@naver.com>
2022-06-24 10:51:41 +0900

    [MyWebPage] feature: 결과 모델 정의 - api 실행 결과에 따른 response와 결과들을 담을 표준 모델들을 생성

---
+++ MyWebPage/API/src/main/java/com/kyy/api/service/model/response/CommonResult.java
@@ -0,0 +1,20 @@
+package com.kyy.api.service.model.response;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/*
+    [api 실행 결과를 담는 모델]
+ */
+@Data
+public class CommonResult {
+    @ApiModelProperty(value = "응답 성공 여부 : true")
+    private boolean success;
+    
+    @ApiModelProperty(value = "응답 코드 번호 : >= 0 정상, < 0 비정상")
+    private int code;
+
+    @ApiModelProperty(value = "응답 메시지")
+    private String msg;
+
+}
