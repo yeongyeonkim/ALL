efa69e9dbd5b05fdd72373c34fbb3d5205203e51
KimYeongYeon <dudtbd111@naver.com>
2022-07-09 16:06:50 +0900

    [MyWebPage] feature: Spring Securiy 적용 및 CSRF 에 대한 이해

--- MyWebPage/API/src/main/java/com/kyy/api/config/DefaultConfig.java
+++ MyWebPage/API/src/main/java/com/kyy/api/config/DefaultConfig.java
@@ -6,22 +6,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-//@EnableCaching
 public class DefaultConfig {
-//    @Bean
-//    public HttpMessageConverters customConverters() {
-//        return new HttpMessageConverters(false,
-//                Collections.<HttpMessageConverter<?>>singleton(new MappingJackson2HttpMessageConverter()));
-//    }
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-//        return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-//                        .allowCredentials(false)
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
--- MyWebPage/API/src/main/java/com/kyy/api/config/JwtConfig.java
+++ MyWebPage/API/src/main/java/com/kyy/api/config/JwtConfig.java
@@ -1,14 +1,14 @@
 package com.kyy.api.config;
 
-import lombok.RequiredArgsConstructor;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@RequiredArgsConstructor
 @Component
 public class JwtConfig {
-    @Value("spring.jwt.secret")
+
+    @Value("${spring.jwt.secret}")
     private String secretKey;
 
-    private long expiredToken = 1000L * 60 * 60;
+    private long expiredToken = 1000L * 60 * 60; // 1시간
+
 }
