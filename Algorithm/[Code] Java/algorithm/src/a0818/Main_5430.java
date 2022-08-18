5e5bef9b82ff4a5b6fb9b9a740ad68351f6baa9d
KimYeongYeon <dudtbd111@naver.com>
2022-08-18 10:37:59 +0900

    [CS] feature: 10 -> 2 진수 변환

---
+++ Algorithm/[Code] Java/algorithm/src/a0818/Main_5430.java
@@ -0,0 +1,12 @@
+package a0818;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_5430 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
